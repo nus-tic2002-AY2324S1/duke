831182efff615d7c1c7bcd3d03fb9da5df59a1bb
yeebinrong <yeebinrong@gmail.com>
2023-08-27 01:15:32 +0800

    level-2 add user commands to list and allow to echo back the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,17 +13,25 @@ public class Duke {
                 + "    \\/  \\/ \\___/|_| |_|_|\\_\\\\__, | |____/ \\___/ \\__|\n"
                 + "                             __/ |                  \n"
                 + "                            |___/                  \n";
-        System.out.println("Wonky: Hello from\n" + logo);
-        System.out.println("Wonky: I'm Wonky the Fairy.");
-        System.out.println("Wonky: What can I do for you?");
+        System.out.println("\tWonky: Hello from\n" + logo);
+        System.out.println("\tWonky: I'm Wonky the Fairy.");
+        System.out.println("\tWonky: What can I do for you?");
 
         String line;
         Scanner in = new Scanner(System.in);
-
+        List<String> enteredTexts = new ArrayList<>();
         while (!(line = in.nextLine()).equals("bye")) {
-            System.out.println("Wonky: " + line);
+            if (line.equals("list")) {
+                System.out.println("\tWonky: Below are the list of items you have added!");
+                for (String enteredText : enteredTexts) {
+                    System.out.println("\t\t" + enteredText);
+                }
+            } else {
+                System.out.println("\tWonky: I have added " + "[" + line + "] to our list!");
+                enteredTexts.add(enteredTexts.size() + 1 + ": " + line);
+            }
         }
-        System.out.println("Wonky: Bye! Thank you for using Wonky Bot. Hope to see you again soon!");
+        System.out.println("\tWonky: Bye! Thank you for using Wonky Bot. Hope to see you again soon!");
         in.close();
     }
 }
