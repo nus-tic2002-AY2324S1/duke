253750c5f1a35e3da9da43f9caac82f81a148b6e
Kelvin <chongkelv@gmail.com>
2023-08-27 14:08:48 +0800

    Extracted method line(), logo() and greet() in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+    }
+
+    private static void greet(){
+        String myChatBotName = "Duke";
+
+        System.out.println(line());
+        System.out.println(logo());
+        System.out.println(line());
+        System.out.printf("Hello! I'm %s\n", myChatBotName);
+        System.out.println("What can I do for you?");
+        System.out.println(line());
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static String line () {
+        String horizontalBox = "─";
+        return horizontalBox.repeat(80);
+    }
+
+    private static String logo(){
+        String logo =
+                " ____        _        \n"
+                        + "|  _ \\ _   _| | _____ \n"
+                        + "| | | | | | | |/ / _ \\\n"
+                        + "| |_| | |_| |   <  __/\n"
+                        + "|____/ \\__,_|_|\\_\\___|\n";
+        return logo;
     }
 }
