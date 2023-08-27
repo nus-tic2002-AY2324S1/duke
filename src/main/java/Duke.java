a6a2ccfc99991aaa82df2c8f5c81a860461f87a1
Kelvin <chongkelv@gmail.com>
2023-08-27 15:37:21 +0800

    Add indentation to the echos from the user input for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,55 +2,69 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        clearScreen();
         greet();
         Scanner in = new Scanner(System.in);
         String input = "";
-        do{
+        do {
             input = in.next();
-            if (input.equalsIgnoreCase("bye")){
+            if (input.equalsIgnoreCase("bye")) {
                 break;
             }
             line();
-            System.out.println(input);
+            echo(input);
             line();
-        }while(!input.equalsIgnoreCase("bye"));
+        } while (!input.equalsIgnoreCase("bye"));
         bye();
-}
-
-    private static void clearScreen(){
-        System.out.print("\033");
     }
 
-    private static void greet(){
+    private static void indentation() {
+        System.out.print("     ");
+    }
+
+    private static void echo(String input){
+        indentation();
+        System.out.println(input);
+    }
+
+    private static void clearScreen() {
+        System.out.println("\033");
+    }
+
+    private static void greet() {
         String myChatBotName = "Luna";
 
         line();
         logo();
         line();
-        System.out.printf("Hello! I'm %s\n", myChatBotName);
-        System.out.println("What can I do for you?");
+        String str = String.format("Hello! I'm %s\n", myChatBotName);
+        echo(str);
+        str = "What can I do for you?";
+        echo(str);
         line();
     }
 
-    private static void line () {
+    private static void line() {
         String horizontalBox = "─";
         String line = horizontalBox.repeat(80);
+        System.out.print("    ");
         System.out.println(line);
     }
 
-    private static void logo(){
+    private static void logo() {
         String logo =
-                  " _                    \n"
-                + "| |    _   _ _  __     __\n"
-                + "| |   | | | | | __  \\/ __ \\\n"
-                + "| |___| |_| | |   | | |__| |\n"
-                + "|_____|\\__,_|_|   |_|_|  |_|\n";
+                "      _                    \n"
+                + "     | |    _   _ _  __     __\n"
+                + "     | |   | | | | | __  \\/ __ \\\n"
+                + "     | |___| |_| | |   | | |__| |\n"
+                + "     |_____|\\__,_|_|   |_|_|  |_|\n";
         System.out.println(logo);
     }
 
-    private static void bye(){
+    private static void bye() {
+        String str = "Bye. Hope to see you again soon!";
         line();
-        System.out.println("Bye. Hope to see you again soon!");
+        echo(str);
         line();
     }
 }
