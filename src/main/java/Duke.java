0cf38ceca05fb976b97951b4109c1e74f1c1cb05
Craby <lethuy1137@gmail.com>
2023-08-28 16:29:51 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
+    public static final String logo = "   ____                  _      __   __\n" +
+            "  / ___|  _ __    __ _  | |__   \\ \\ / /\n" +
+            " | |     | '__|  / _` | | '_ \\   \\ V / \n" +
+            " | |___  | |    | (_| | | |_) |   | |  \n" +
+            "  \\____| |_|     \\__,_| |_.__/    |_|  \n" +
+            "                                       \n";
+    public static final String crab = "              __       __\n" +
+            "             /           \\\n" +
+            "            (  / @   @ \\  )\n" +
+            "             \\(_ _\\_/_ _)/\n" +
+            "           (\\ `-/     \\-' /)\n" +
+            "            \"===\\     /===\"\n" +
+            "             .==')___(`==.       \n" +
+            "              .='     `=.";
+    public static final String line = "  _____________________________________";
+    public static final String hello = "   Hello! I'm CrabY 🦀 \n" + "   What is your name?";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println(line);
+        System.out.println(hello);
+        System.out.println(line);
+        System.out.println("   Bye. Hope to see you again soon!");
+        System.out.println(line);
+        System.out.println(crab);
     }
 }
