916f46f4a4d371ff2224dad0bd6c003eb70e99a3
gwee melvin <gwee_melvin@live.com>
2023-08-29 19:57:36 +0800

    Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
+    public static void greetings(){
+        printDashes();
+        System.out.println("Hello I'm Tim\nWhat can I do for you?");
+        printDashes();
+    }
+    public static void printDashes(){
+        System.out.println();
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void goodbyeGreet(){
+        System.out.println("Bye. Hope to see you again soon!");
+        printDashes();
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo =
+                    " _______                 \n"
+                +   "|__   __| [ ]  __  __    \n"
+                +   "   | |    | | |  \\/  |  \n"
+                +   "   | |    | | | |\\/| |  \n"
+                +   "   |_|    |_| |_|  |_|   \n";
         System.out.println("Hello from\n" + logo);
+        greetings();
+        goodbyeGreet();
     }
 }
