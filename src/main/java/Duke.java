1ac25b0b61606340831ce8a7910703480022a217
Craby <lethuy1137@gmail.com>
2023-09-01 06:04:10 +0800

    Mark as Done, cleaned up some code line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,56 +19,64 @@ public class Duke {
             "              .='     `=.";
     public static final String line = "  _____________________________________";
     public static final String hello = "   Hello! I'm CrabY 🦀 \n" + "   What is your name?";
-    public static void addList(List<String> listTodo, String answer) {
-        listTodo.add(answer);
-    }
 
     public static void main(String[] args) {
-        System.out.println(line);
-        System.out.println(logo);
-        System.out.println(hello);
-        System.out.println(line);
+        System.out.println(line + "\n" + logo + "\n" + hello + "\n" + line);
         Scanner userName = new Scanner(System.in);
         String answerName = userName.nextLine();
-        System.out.println(line);
-        System.out.println("   Hi " + answerName + ", How can I help you today?");
-        System.out.println(line);
+        System.out.println(line + "\n" + "   Hi " + answerName + ", How can I help you today?" + "\n" + line);
         Scanner userAnswer = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
+
         while (true) {
             boolean exitW = false;
             String answer2 = userAnswer.nextLine();
-            String answer1 = answer2.toLowerCase();
+            String answer1 = answer2.split(" ")[0];
+            answer1 = answer1.toLowerCase();
             System.out.println(line);
-
             switch (answer1) {
                 case "list":
                     int i = 0;
                     System.out.println("   Here are the tasks in your list:");
-                    for (String s : list) {
+                    for (Task s : tasks) {
                         i++;
                         System.out.println("   " + i + ". " + s);
                     }
                     System.out.println(line);
                     break;
                 case "blah":
-                    System.out.println("   blah");
-                    System.out.println(line);
+                    System.out.println("   blah \n" + line);
                     break;
                 case "bye":
                     System.out.println("   Bye " + answerName + ", hope to see you again soon!");
                     exitW = true;
                     break;
+                case "mark":
+                    int a = (Integer.parseInt(answer2.split(" ")[1])) - 1;
+                    if (a >= tasks.size() || a < 0){
+                        System.out.println("   Opp, something wrong! Your list only have 1 to " + tasks.size() + " tasks. \n" + "   Please try again! \n" + line);
+                        break;
+                    }
+                    tasks.get(a).setDone(true);
+                    System.out.println("Nice! I've marked this task as done:" + "\n" + "   " + tasks.get(a) + "\n" + line);
+                    break;
+                case "unmark":
+                    int b = (Integer.parseInt(answer2.split(" ")[1])) - 1;
+                    if (b >= tasks.size() || b < 0){
+                        System.out.println("   Opp, something wrong! Your list only have 1 to " + tasks.size() + "tasks. \n" + "   Please try again! \n" + line);
+                        break;
+                    }
+                    tasks.get(b).setDone(false);
+                    System.out.println("   OK, I've marked this task as not done yet:" + "\n" + "   " + tasks.get(b) + "\n" + line);
+                    break;
                 default:
-                    addList(list, answer2);
-                    System.out.println("   added: " + answer2 + " - to your list");
-                    System.out.println(line);
+                    tasks.add(new Task(answer2));
+                    System.out.println("   added: " + answer2 + " - to your list. \n" + line);
             }
             if (exitW) {
                 break;
             }
         }
-        System.out.println(line);
-        System.out.println(crab);
+        System.out.println(line + "\n" + crab);
     }
 }
