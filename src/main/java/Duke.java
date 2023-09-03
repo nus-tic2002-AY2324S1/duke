a72a60265f1253fb29218f99692816665e6e6148
Ardian <>
2023-09-03 15:26:33 +0800

    personalised bot name, added welcome & farewell message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("────────────────────────────────────────");
+        System.out.println("Hello! I'm Botzy!\nWhat can I do for you?");
+        System.out.println("────────────────────────────────────────");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("────────────────────────────────────────");
     }
 }
