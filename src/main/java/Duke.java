45537d6c2da8dcbd52e5b97cf78e1d9cfb583685
Ardian <>
2023-09-03 16:01:34 +0800

    implemented welcomeMessage, byeMessage, echoMessage method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,41 @@
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.Objects;
+import java.util.Scanner;
+
 
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
-        System.out.println("────────────────────────────────────────");
-        System.out.println("Hello! I'm Botzy!\nWhat can I do for you?");
-        System.out.println("────────────────────────────────────────");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("────────────────────────────────────────");
+        Scanner in = new Scanner(System.in);
+
+        welcomeMessage();
+
+        while(true) {
+            String input = in.nextLine();
+            if (Objects.equals(input, "bye")) {
+                byeMessage();
+                break;
+            } else {
+                echoMessage(input);
+            }
+        }
+    }
+
+    public static void welcomeMessage() {
+        System.out.println("\t────────────────────────────────────────");
+        System.out.println("\tHello! I'm Botzy!\n\tWhat can I do for you?");
+        System.out.println("\t────────────────────────────────────────");
+    }
+
+    public static void byeMessage() {
+        System.out.println("\t────────────────────────────────────────");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t────────────────────────────────────────");
+    }
+
+    public static void echoMessage(String args) {
+        System.out.println("\t────────────────────────────────────────");
+        System.out.println("\t" + args);
+        System.out.println("\t────────────────────────────────────────");
     }
 }
