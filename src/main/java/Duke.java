08c3dd474f60fcefae1364d3d277ab6cd8fc0911
CHUBB-DESKTOP\caleb <caleb.young@gmail.com>
2023-09-03 18:47:34 +0800

    Created Task class Revised code to use Task Class. Update Logo Added mark function - covers both mark and unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,57 @@
 import java.util.Scanner;
 public class Duke {
-    private static String[] list = new String[100];
+    private static Task[] list = new Task[100];
     private static int listCount =0;
 
-    public static void addMessage(String msg){
-        list[listCount] = msg;
+    public static void addTask(String msg){
+        list[listCount] = new Task(msg);
         listCount++;
         System.out.println("added: " + msg);
     }
+
     public static void printList(){
+        System.out.println("Here are the tasks in your list:");
         for (int i=0; i<listCount; i++){
-            System.out.println( i+1 +". " + list[i]);
+            System.out.println( (i+1) + ". [" + list[i].getStatusIcon() + "] "
+                + list[i].getDescription());
         }
     }
 
+    public static void mark(int item, boolean isDone){
+        if (item > listCount) {return;}
+
+        list[item - 1].setIsDone(isDone);
+        if (isDone){
+            System.out.println("Nice! I've marked this task as done:");
+        } else {
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.println( "[" + list[item - 1].getStatusIcon() + "] "
+                + list[item - 1].getDescription());
+    }
+
     public static void main(String[] args) {
-        //String logo = " ____        _        \n"
-        //        + "|  _ \\ _   _| | _____ \n"
-        //        + "| | | | | | | |/ / _ \\\n"
-        //        + "| |_| | |_| |   <  __/\n"
-        //       + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
+        String logo = " ____            ____ \n"
+                    + "|      __   __  |    \n"
+                    + "|____ \\  | |  / |____\n"
+                    + "|      \\ \\_/ /  |    \n"
+                    + "|____   \\___/   |____ \n";
         String line;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Hello I'm Eve!\nWhat can I do for you?");
+        System.out.println("Hello I'm Eve!\n" + logo + "\nWhat can I do for you?");
 
         for (;;){
             line = in.nextLine();
-            if (line.equals("bye")) {
+            String[] words = line.split(" ");
+            if (words[0].equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
-            } else if (line.equals("list")) {
-                printList();
+            } else if (words[0].equals("list")) { printList();
+            } else if (words[0].equals("mark")) { mark(Integer.parseInt(words[1]),true);
+            } else if (words[0].equals("unmark")) { mark(Integer.parseInt(words[1]), false);
             } else {
-                addMessage(line);
+                addTask(line);
             }
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() { return (isDone ? "X" : " "); }
+    public String getDescription() { return this.description; }
+    public void setIsDone(boolean isDone){ this.isDone = isDone; }
+
+}
