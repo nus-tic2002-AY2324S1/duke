2870c1c790159a9bfa76faf4f70d8f34e06d1d56
CHUBB-DESKTOP\caleb <caleb.young@gmail.com>
2023-09-03 17:38:25 +0800

    Implemented infinite loop with exit on "bye" Echo for all other text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         //String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        //        + "|  _ \\ _   _| | _____ \n"
+        //        + "| | | | | | | |/ / _ \\\n"
+        //        + "| |_| | |_| |   <  __/\n"
+        //       + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
-        System.out.println("--------------------------------------");
-        System.out.println("Hello I'm Eve!\nWhat can I do for you?");
-        System.out.println("--------------------------------------");
+        String line;
+        Scanner in = new Scanner(System.in);
 
-        System.out.println("--------------------------------------");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("--------------------------------------");
+        System.out.println("Hello I'm Eve!\nWhat can I do for you?");
+
+        for (;;){
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(line);
+            }
+        }
+
     }
 }
