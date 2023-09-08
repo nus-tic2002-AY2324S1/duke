87cb1e7a54afd25b3f7b127a3f532b783e19822b
Kai Siang <113116804+Case000@users.noreply.github.com>
2023-09-08 17:59:04 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String echo = new String("test");
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm CLARA \n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n";
+
+        String bye = "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(greeting + bye);
     }
 }
