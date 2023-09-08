2a476bd1bb5fe7d44abb1f8df9e292f2c3cb00bd
Jac <e0895980@u.nus.edu>
2023-09-08 14:16:11 +0800

    echo commands and exits on 'bye'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("GREETINGS PROFESSOR FALKEN.\n\n"
                             + "SHALL WE PLAY A GAME?\n");
-        System.out.println(">> ");
-        System.out.println("GOODBYE.\n");
+        String input;
+        do {
+            System.out.print(">> ");
+            input = getUserInput();
+            joshuaSays(input);
+        } while (!input.equals("bye"));
+
+        System.out.println("\nGOODBYE.\n");
+    }
+
+    public static String getUserInput(){
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        line = line.toLowerCase();
+        return line;
+    }
+
+    public static void joshuaSays(String str){
+        str = str.toUpperCase();
+        System.out.println(str + "\n");
     }
 }
