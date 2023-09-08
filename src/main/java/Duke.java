9f5df16ddd8d9ca2cd7908d2a5a2010af443e6d3
qwpeh <e0493846@u.nus.edu>
2023-09-08 17:41:57 +0800

    Completed Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,19 +13,41 @@ public class Duke {
         String echo = "";
         String[] list = new String[100];
         Scanner typed = new Scanner(System.in);
+        String[] instructions;
         int count = 0;
-        while(!echo.equals("bye")){
+        int store = 0;
+        String[] updates = new String[100];
+        while(!echo.matches("bye(.*)")){
             echo = typed.nextLine();
-            if(!echo.equals("bye") && !echo.equals("list")){
+            if(echo.matches("mark(.*)") && echo.contains(" ")){
+                instructions = echo.split(" ");
+                store = Integer.parseInt(instructions[1]);
+                if(list.length >= store){
+                    //print
+                    updates[store-1] = "X";
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + updates[store-1] + "]" + " " + list[store-1]);
+                }
+            }else if(echo.matches("unmark(.*)") && echo.contains(" ")){
+                instructions = echo.split(" ");
+                store = Integer.parseInt(instructions[1]);
+                if(list.length >= store){
+                    //print
+                    updates[store-1] = " ";
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[" + updates[store-1] + "]" + " " + list[store-1]);
+                }
+            }else if(!echo.equals("bye") && !echo.equals("list") && !echo.equals("")){
                 //add to array:
                 list[count] = echo;
+                updates[count] = " ";
                 count++;
                 //System.out.println(echo);
                 System.out.println("added: " + echo);
-            } else if (echo.equals("list")) {
+            }else if (echo.matches("list(.*)")) {
                 for(int i = 1; i<= list.length; i++){
                     if(list[i-1]!=null) {
-                        System.out.println(i + ". " + list[i - 1]);
+                        System.out.println(i + "." + "[" + updates[i-1] + "]" + " " + list[i - 1]);
                     }else{
                         break;
                     }
