dca2a1e7dda9cb60a28e016f8631c8b85b473762
Kelvin <chongkelv@gmail.com>
2023-09-09 23:07:48 +0800

    Add Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,21 @@
+public class Deadline extends Task{
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+        abbreviation = 'D';
+
+        responseGotIt();
+        Duke.indentation();
+        System.out.print(" ");
+        System.out.println(this);
+        responseNumberOfTasks();
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("[%c][%s] %s (by: %s)",abbreviation,getStatusIcon(),description, by);
+    }
+}
