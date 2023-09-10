8a1497a06b39a99ca58e1f424ade7b0a57967e7e
vennica li <vennica0521@gmail.com>
2023-09-10 16:52:38 +0800

    Level-1 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,10 +11,21 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________\n"+
                 "Hello! I'm venni\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________"
+                "What can I do for you?\n"
         );
+
+        String input = scanner.nextLine();
+        while(!input.equalsIgnoreCase("bye")){
+            System.out.println(
+                    "    ____________________________________________________________\n" +
+                    "     " + input +
+                    "    ____________________________________________________________\n" );
+            input = scanner.nextLine();
+        }
+
+        if (input.equalsIgnoreCase("bye")){
+            System.out.println("Bye. Hope to see you again soon!\n" +
+                                "____________________________________________________________");
+        }
     }
 }
