e8415533e8c4596978418516ff14727a2739e9d5
Kelvin <chongkelv@gmail.com>
2023-09-10 16:03:47 +0800

    Add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,22 @@
+public class Event extends Task{
+
+    protected String from;
+    protected String to;
+    public Event(String description, String from, String to){
+        super(description);
+        this.from = from;
+        this.to = to;
+        abbreviation = 'E';
+
+        responseGotIt();
+        Duke.indentation();
+        System.out.print(" ");
+        System.out.println(this);
+        responseNumberOfTasks();
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[%c][%s] %s (from: %s to: %s)",abbreviation,getStatusIcon(),description, from, to);
+    }
+}
