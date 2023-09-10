36cc3c02d87a31c056b6b79ebbd4bf6222cfa550
Ziming <gzm98727@gmail.com>
2023-09-10 20:25:02 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String lineBreak = "****************************************";
         String logo ="\n" +
                 "╭━━━╮╱╱╱╱╱╭╮\n" +
                 "┃╭━╮┃╱╱╱╱╱┃┃\n" +
@@ -11,23 +14,39 @@ public class Duke {
                 "╰━━━┻━━┫╭━┻╯╰┻━━┻╯╰╯\n" +
                 "╱╱╱╱╱╱╱┃┃\n" +
                 "╱╱╱╱╱╱╱╰╯\n";
-        String lineBreak = "****************************************";
+
+        // Greet the user and ask for input
         System.out.println(logo+"Hello, I'm Sophon:). \nHow can i assist you today?");
         System.out.println(lineBreak);
 
-        Scanner scanner = new Scanner(System.in);
+        // Create an array of string to store user's input
+        String[] userInputArray = new String[100];
+        // Counter to store the number of input stored in the array
+        int n = 0;
+
         while(true){
             String userInput = scanner.nextLine();
             if(Objects.equals(userInput.toUpperCase(), "BYE")){
                 System.out.println(lineBreak);
-                System.out.println("Bye. See you next time!");
+                System.out.println("Bye! Hope to see you again soon!");
                 System.out.println(lineBreak);
                 break;
             }else{
-                System.out.println(lineBreak);
-                System.out.println(userInput);
-                System.out.println(lineBreak);
+                if(Objects.equals(userInput.toUpperCase(),"LIST")){
+                    System.out.println(lineBreak);
+                    for (int i = 0; i < n; i++) {
+                        System.out.println((i + 1)+ ". " + userInputArray[i]);
+                    }
+                    System.out.println(lineBreak);
+                }else{
+                    userInputArray[n] = userInput;
+                    n++;
+                    System.out.println(lineBreak);
+                    System.out.println("Added: " + userInput);
+                    System.out.println(lineBreak);
+                }
             }
         }
+        scanner.close();
     }
 }
