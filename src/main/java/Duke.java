19e23e29f978734e243dee23227d51e519799a61
Ziming <gzm98727@gmail.com>
2023-09-10 20:20:12 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo ="\n" +
@@ -12,5 +14,20 @@ public class Duke {
         String lineBreak = "****************************************";
         System.out.println(logo+"Hello, I'm Sophon:). \nHow can i assist you today?");
         System.out.println(lineBreak);
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String userInput = scanner.nextLine();
+            if(Objects.equals(userInput.toUpperCase(), "BYE")){
+                System.out.println(lineBreak);
+                System.out.println("Bye. See you next time!");
+                System.out.println(lineBreak);
+                break;
+            }else{
+                System.out.println(lineBreak);
+                System.out.println(userInput);
+                System.out.println(lineBreak);
+            }
+        }
     }
 }
