2994bcf19bb520016cf4406e8366914dace29655
agqy98 <anxelxoh@gmail.com>
2023-09-14 12:36:24 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    private static int count = 0;
+    private static String[] list = new String[100];
+
     public static void main(String[] args) {
         printHelloWorld();
 
@@ -8,7 +12,11 @@ public class Duke {
         System.out.print("");
         String input = scanner.nextLine();
         while (!input.equalsIgnoreCase("bye")) {
-            printRepeat(input);
+            if (input.equalsIgnoreCase("list")) {
+                printList();
+            } else {
+                handleInput(input);
+            }
             input = scanner.nextLine();
         }
         printByeWorld();
@@ -19,16 +27,29 @@ public class Duke {
         System.out.print("    ");
     }
 
+    private static void printList() {
+        printSeparator();
+        for (int i = 0; i < count; i++) {
+            formatter();
+            System.out.print(i + 1);
+            System.out.print(". ");
+            System.out.println(list[i]);
+        }
+        printSeparator();
+    }
+
+    private static void handleInput(String input) {
+        printSeparator();
+        formatter();
+        list[count] = input;
+        count++;
+        System.out.println("added: " + input);
+        printSeparator();
+    }
+
     private static void printSeparator() {
         formatter();
         System.out.println("____________________________________________________________");
-    }
-
-    private static void printRepeat(String msg) {
-        printSeparator();
-        formatter();
-        System.out.println(msg);
-        printSeparator();
     }
 
     private static void printHelloWorld() {
