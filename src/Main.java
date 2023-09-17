a097b91aa7b3d007985564c69f6c50f06960533b
jy0251 <123530247+jy0251@users.noreply.github.com>
2023-09-17 16:58:43 +0800

    Level 0. Rename, Greet, Exit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Greet the user
+        String logo = "    ____                       __\n" +
+                "   / __ \\__  ______  ___  ____/ /\n" +
+                "  / / / / / / / __ \\/ _ \\/ __  /\n" +
+                " / /_/ / /_/ / /_/ /  __/ /_/ /\n" +
+                "/_____/\\__,_/ .___/\\___/\\__,_/\n" +
+                "           /_/";
+        System.out.println("Hello! I'm DupeBot.\n" + logo);
+        System.out.println("What can I do for you?");
+
+        // Read user input
+        String userInput = scanner.nextLine();
+
+        // Check if the user wants to exit
+        if (userInput.equalsIgnoreCase("exit")) {
+            // Say goodbye and exit the program
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            // For any other input, you can provide a response (you can expand on this)
+            System.out.println("I'm a simple chatbot and the chat ends here. Bye.");
         }
+
+        // Close the scanner
+        scanner.close();
     }
 }
