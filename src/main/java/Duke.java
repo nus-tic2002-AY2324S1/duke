6ea127766324cba716105afe4a9d891195d8037e
CXinCheng <Smilence97@outlook.com>
2023-09-19 17:32:44 +0800

    Echo whatever user input and exit with 'bye'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "████████ ██ ███    ██  █████  \n" +
@@ -9,6 +11,21 @@ public class Duke {
         System.out.println("Hello! I'm TINA. \nHow can I help you?\n");
         System.out.println("Type your question below. (Type \"bye\" to exit)");
         System.out.println("**************************************************\n");
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = "";
+
+        while (true) {
+            userInput = sc.nextLine();
+            System.out.println("    **************************************************");
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("    " + userInput);
+            System.out.println("    **************************************************\n");
+        }
+
+        System.out.println("    Bye. Hope to see you again.\n");
     }
 }
 
