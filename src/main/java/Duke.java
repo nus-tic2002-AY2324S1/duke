4a911ae15acac03148fb3182a654e0bc853c1351
qwpeh <e0493846@u.nus.edu>
2023-09-22 13:16:00 +0800

    Completed Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "__________              .__    .__ \n"
@@ -11,7 +15,7 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         String echo = "";
-        String[] list = new String[100];
+        ArrayList<String> list = new ArrayList<String>();
         Scanner typed = new Scanner(System.in);
         String[] instructions;
         int count = 0;
@@ -21,6 +25,8 @@ public class Duke {
         String[] by = new String[100];
         String[] event = new String[100];
         String[] event2 = new String[100];
+        ArrayList<String> list1 = new ArrayList<String>();
+
 
         while (!echo.matches("bye(.*)")) {
                     echo = typed.nextLine();
@@ -30,18 +36,18 @@ public class Duke {
                             event2 = event[1].split("/to");
                             from = event2[0];
                             to = event2[1];
-                            list[count] = "[E]" + "[ ] " + event[0] + "(from:" + from + " to:" + to + ")";
+                            list.add("[E]" + "[ ] " + event[0] + "(from:" + from + " to:" + to + ")");
                             //list[i-1][4] change for mark&unmark [X] [ ]
                             //i is counter for printing 1. 2. 3.
                             System.out.println("Got it. I've added this task:");
-                            System.out.println(list[count]);
+                            System.out.println(list.get(count));
                             count++;
                             System.out.println("Now you have " + count + " tasks in the list.");
                         } else if (echo.contains("/by")) {
                             by = echo.split("/by");
-                            list[count] = "[D][ ] " + by[0] + "(by:" + by[1] + ")";
+                            list.add("[D][ ] " + by[0] + "(by:" + by[1] + ")");
                             System.out.println("Got it. I've added this task:");
-                            System.out.println(list[count]);
+                            System.out.println(list.get(count));
                             count++;
                             System.out.println("Now you have " + count + " tasks in the list.");
 
@@ -51,34 +57,43 @@ public class Duke {
                         }else if(echo.matches("to") || echo.matches("do") || echo.matches("todo")){
                             System.out.println("OOPS!!! The description of a todo cannot be empty.");
                             System.out.println("please include what to do in a todo");
-                        } else if (!echo.matches("mark(.*)") && !echo.matches("unmark(.*)")) {
-                            list[count] = "[T][ ] " + echo;
+                        } else if (!echo.matches("mark(.*)") && !echo.matches("unmark(.*)") && !echo.matches("delete(.*)")) {
+                            list.add("[T][ ] " + echo);
                             System.out.println("Got it. I've added this task:");
-                            System.out.println(list[count]);
+                            System.out.println(list.get(count));
                             count++;
                             System.out.println("Now you have " + count + " tasks in the list.");
-                        } else if (echo.matches("mark(.*)") && echo.contains(" ") && list[0] != null) {
+                        } else if (echo.matches("mark(.*)") && echo.contains(" ") && !list.isEmpty()) {
                             instructions = echo.split(" ");
                             store = Integer.parseInt(instructions[1]); //position number start from 1
-                            if (list.length >= store) {
-                                list[store - 1] = list[store - 1].replace("[ ]", "[X]");
+                            if (list.size() >= store) {
+                                list.set(store-1, (list.get(store-1).substring(0,4) + "X" + list.get(store-1).substring(5))); //.replace
                                 System.out.println("Nice! I've marked this task as done:");
-                                System.out.println(list[store - 1]);
+                                System.out.println(list.get(store-1));
                             }
-                        } else if (echo.matches("unmark(.*)") && echo.contains(" ") && list[0] != null) {
+                        } else if (echo.matches("unmark(.*)") && echo.contains(" ") && !list.isEmpty()) {
                             instructions = echo.split(" ");
                             store = Integer.parseInt(instructions[1]); //position number start from 1
-                            if (list.length >= store) {
-                                list[store - 1] = list[store - 1].replace("[X]", "[ ]");
+                            if (list.size() >= store) {
+                                list.set(store-1, (list.get(store-1).substring(0,4) + " " + list.get(store-1).substring(5)));
                                 System.out.println("OK, I've marked this task as not done yet:");
-                                System.out.println(list[store - 1]);
+                                System.out.println(list.get(store-1));
+                            }
+                        }else if (echo.matches("delete(.*)") && echo.contains(" ") && !list.isEmpty()){
+                            instructions = echo.split(" ");
+                            store = Integer.parseInt(instructions[1]);
+                            if(list.size() >= store){
+                                System.out.println("Noted. I've removed this task:");
+                                System.out.println("    " + list.get(store-1));
+                                list.remove(store-1);
+                                System.out.println("Now you have " + list.size() + " tasks in the list.");
                             }
                         }
 
                     } else if (echo.matches("list")) {
-                        for (int i = 1; i <= list.length; i++) {
-                            if (list[i - 1] != null) {
-                                System.out.println(i + "." + list[i - 1]);
+                        for (int i = 1; i <= list.size(); i++) {
+                            if (list.get(i - 1) != null) {
+                                System.out.println(i + "." + list.get(i - 1));
                             } else {
                                 break;
                             }
