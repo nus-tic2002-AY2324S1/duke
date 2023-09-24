bdc3ad4501a3efdf6dc847aa28e21c62c8edf8f1
Craby <lethuy1137@gmail.com>
2023-09-24 18:02:34 +0800

    add deleteall command

--- src/main/java/command/DeleteAllCommand.java
+++ src/main/java/command/DeleteAllCommand.java
@@ -4,9 +4,12 @@ import task.Task;
 
 import java.util.List;
 
-public class DeleteAllCommand extends CrabyMessage implements CommandInterface{
+public class DeleteAllCommand extends CrabyMessage implements CommandInterface {
     @Override
     public void handleCommand(String input, List<Task> tasks) {
-
+        System.out.println(blank + "✂ Ok, I clean up all your tasks");
+        System.out.println(blank + "╰┈➤ Let's start a new checklist");
+        tasks.clear();
+        System.out.println(line);
     }
 }
