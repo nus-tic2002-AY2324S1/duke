fb8a8777c931d170a4b6111d850b0eec464fc6fd
chongkelv <e0895806@u.nus.edu>
2023-10-23 14:57:08 +0900

    Add JavaDoc to Todo class.

--- src/main/java/duke/task/Todo.java
+++ src/main/java/duke/task/Todo.java
@@ -3,11 +3,27 @@ package duke.task;
 import duke.exception.InvalidArgumentException;
 
 public class Todo extends Task {
+    /**
+     * Construction
+     */
+    public Todo(){
+        isDone = false;
+    }
+
+    /**
+     * Construction of Todo task take in description sets its abbreviation
+     * @param description The description for the Todo task.
+     */
     public Todo(String description)  {
         super(description);
         setAbbreviation();
     }
 
+    /**
+     * Construction of Todo task take in description and status of the task sets its abbreviation.
+     * @param isDone The status of the task.
+     * @param description The description for the Todo task.
+     */
     public Todo(boolean isDone, String description){
         super(description);
         setAbbreviation();
@@ -18,14 +34,18 @@ public class Todo extends Task {
         response();
     }
 
+    /**
+     * set abbreviation 'T'
+     */
     public void setAbbreviation() {
         abbreviation = 'T';
     }
 
-    public Todo(){
-        isDone = false;
-    }
+    
 
+    /**
+     * Override toString method to print detail relevant to task.
+     */
     @Override
     public String toString() {
         return String.format("[%c][%s] %s",abbreviation,getStatusIcon(),dukeDescription);
