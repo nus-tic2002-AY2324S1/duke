1008b759a2516ac902bcab2f74a957cf8c4eeb3d
chongkelv <e0895806@u.nus.edu>
2023-10-23 18:19:16 +0900

    Add JavaDoc to TaskDecoder class

--- src/main/java/duke/storage/TaskDecoder.java
+++ src/main/java/duke/storage/TaskDecoder.java
@@ -16,6 +16,12 @@ import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 
 public class TaskDecoder {
+    /**
+     * Decodes a list of strings into Task objects and returns them as an ArrayList.
+     * @param strings The list of strings representing tasks to be decoded.
+     * @return An ArrayList containing Task objects decoded from the input strings.
+     * @throws FileStorageException If there are issues with decoding the tasks.
+     */
     public static ArrayList<Task> decodeTask(List<String> strings) throws FileStorageException{
         ArrayList<Task> tasks = new ArrayList<>();
         for (String string: strings) {
@@ -23,7 +29,12 @@ public class TaskDecoder {
         }
         return tasks;
     }
-
+    /**
+     * Decodes a string representation of a task and returns the corresponding Task object.
+     * @param string The string representation of the task to be decoded.
+     * @return The Task object decoded from the input string.
+     * @throws FileStorageException If there are issues with the format or parsing of the string.
+     */
     private static Task decodeTaskFromString(String string) throws FileStorageException{
         char abbreviation = string.charAt(0);
         String[] tokens = getTokens(string);
@@ -58,6 +69,11 @@ public class TaskDecoder {
                 Message.MESSAGE_MAKE_NEW_INSTANCE);
         throw new FileStorageException(message);
     }
+    /**
+     * Splits the input string into tokens using the "|" delimiter and returns the tokens as an array.
+     * @param string The input string to be split into tokens.
+     * @return An array of strings representing the tokens extracted from the input string.
+     */
     private static String[] getTokens (String string) {
         List<String> tokens = new ArrayList<>();
         StringTokenizer stringTokenizer = new StringTokenizer(string, "|");
@@ -67,7 +83,11 @@ public class TaskDecoder {
         String[] arr = new String[tokens.size()];
         return tokens.toArray(arr);
     }
-
+    /**
+     * Parses a string argument representing a date and time and returns a LocalDateTime object.
+     * @param byArgument The string argument representing the date and time.
+     * @return The LocalDateTime object parsed from the input string, or null if parsing fails.
+     */
     public static LocalDateTime getDateTime(String byArgument){
         final Matcher dateMatcher = Command.DATE_ARG_FORMAT.matcher(byArgument);
         if(!dateMatcher.matches()){
