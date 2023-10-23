d6613d69ee4ebc6724ccbf269b3e69609322400e
jy0251 <123530247+jy0251@users.noreply.github.com>
2023-10-24 00:20:36 +0800

    added task marker, created task class.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+    private static final int MAX_TASKS = 100;
+    private static Task[] tasks = new Task[MAX_TASKS];
+    private static int taskCounter = 0;
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String [] tasks = new String[100];
-        int taskCounter = 0;
 
         // Greet the user
         String logo = "    ____                       __\n" +
@@ -22,38 +21,82 @@ public class Main {
         System.out.println("Hello! I'm DupeBot.\n" + logo);
         System.out.println("What can I do for you?");
 
-        while (true) {
-            // Read user input
-            String userInput = scanner.nextLine();
 
-            // Check if the user wants to exit
+        while (true) {
+            String userInput = scanner.nextLine();
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                break; // Exit the loop and end the program
+                break;
             } else if (userInput.equalsIgnoreCase("list")) {
-                // if user inputs list, then print out list if task list is not empty
-                if(taskCounter == 0){
-                    System.out.println("the task list is currently empty");
-                }
-                else {
-                    for (int i = 0; i < taskCounter; i++){
-                        System.out.println((i+1) + ". " + tasks[i]);
-                    }
-                }
-            }
-            else {
-                // if user inputs anything else, then insert into list. check if list does not exceed 100
-                if (taskCounter < 100) {
-                    tasks[taskCounter] = userInput;
-                    taskCounter++;
-                    System.out.println("added: " + userInput);
-                } else {
-                    System.out.println("Sorry, your task list is full.");
-                }
+                displayTasks();
+            } else if (userInput.startsWith("mark ")) {
+                markTaskAsDone(userInput);
+            } else if (userInput.startsWith("unmark ")) {
+                unmarkTask(userInput);
+            } else {
+                addTask(userInput);
             }
         }
 
-        // Close the scanner
         scanner.close();
     }
+
+    private static void displayTasks() {
+        if (taskCounter == 0) {
+            System.out.println("The task list is currently empty");
+        } else {
+            for (int i = 0; i < taskCounter; i++) {
+                Task task = tasks[i];
+                System.out.println((i + 1) + " " + task.getStatusIcon() + " " + task.getDescription());
+            }
+        }
+    }
+
+    private static void markTaskAsDone(String userInput) {
+        int taskIndex = getTaskIndex(userInput);
+        if (taskIndex >= 1 && taskIndex <= taskCounter) {
+            Task task = tasks[taskIndex - 1];
+            task.markAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+        } else {
+            System.out.println("Invalid task number.");
+        }
+    }
+
+    private static void unmarkTask(String userInput) {
+        int taskIndex = getTaskIndex(userInput);
+        if (taskIndex >= 1 && taskIndex <= taskCounter) {
+            Task task = tasks[taskIndex - 1];
+            task.markAsNotDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+        } else {
+            System.out.println("Invalid task number.");
+        }
+    }
+
+    private static int getTaskIndex(String userInput) {
+        String[] parts = userInput.split(" ");
+        if (parts.length == 2) {
+            try {
+                return Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                // Handle the case where the user didn't provide a valid number.
+            }
+        }
+        return -1; // Invalid input
+    }
+
+    private static void addTask(String description) {
+        if (taskCounter < MAX_TASKS) {
+            Task task = new Task(description);
+            tasks[taskCounter] = task;
+            taskCounter++;
+            System.out.println("Added: " + description);
+        } else {
+            System.out.println("Sorry, your task list is full.");
+        }
+    }
 }
+
