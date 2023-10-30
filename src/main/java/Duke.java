f0206b34ec000dd3b04b2332061b6425f97eeb3d
proseraju <ponnuroseraju@gmail.com>
2023-10-30 15:51:00 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        // Rename your chatbot
+        // Rename the chatbot
         String chatbotName = "RoboChat";
 
         // Greet the user
@@ -10,6 +10,22 @@ public class Duke {
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you?");
         System.out.println ("--------------------------------------------------------");
+
+
+        // Create a Scanner object for user input
+        Scanner userInput = new Scanner(System.in);
+        String userCommand;
+            do {
+                System.out.print("Enter your command: ");
+                userCommand  = userInput.nextLine();
+
+                if (userCommand.equalsIgnoreCase("bye")){
+                    break;
+                }
+                System.out.println("The command entered by the user is: " + userCommand);
+                System.out.println ("--------------------------------------------------------");
+
+            }while(!userCommand.equalsIgnoreCase("bye"));
 
         // Exit
         System.out.println("Bye. Hope to see you again soon!");
