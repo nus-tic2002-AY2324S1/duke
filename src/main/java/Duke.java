03e02ec108b679b1a32ac880a3795746d3e92cc7
proseraju <ponnuroseraju@gmail.com>
2023-11-01 13:54:02 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -47,13 +47,59 @@ public class Duke {
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println(t.getStatusIcon() + " " + t.getDescription());
 
-                // list all added items if user command is "list"
+
+                } else if (userCommand.startsWith("todo ")) {
+                    String description = userCommand.substring(5);
+                    Todo t = new Todo(description);
+                    list.add(t);
+                    System.out.println("Got it. I've added this task:\n  " + t);
+                    System.out.println("Now you have " + list.size() + " tasks in the list.");
+                    System.out.println ("--------------------------------------------------------");
+
+                }   else if (userCommand.startsWith("deadline ")) {
+                        // Parse the description and deadline details here
+                        String[] parts = userCommand.substring(9).split(" /by ");
+                        if (parts.length == 2) {
+                            String description = parts[0];
+                            String by = parts[1];
+                            Deadline d = new Deadline(description, by);
+                            list.add(d);
+                            System.out.println("Got it. I've added this task:\n  " + d);
+                            System.out.println("Now you have " + list.size() + " tasks in the list.");
+                            System.out.println ("--------------------------------------------------------");
+                        } else {
+                            System.out.println("Invalid deadline format.");
+                        }
+                    }
+
+                    else if (userCommand.startsWith("event ")) {
+                        // Parse the description and event details here
+                        String[] parts = userCommand.substring(6).split(" /from ");
+                        if (parts.length == 2) {
+                            String description = parts[0];
+                            String[] time = parts[1].split(" /to ");
+                            if (time.length == 2) {
+                                String start = time[0];
+                                String end = time[1];
+                                Event e = new Event(description, start, end);
+                                list.add(e);
+                                System.out.println("Got it. I've added this task:\n  " + e);
+                                System.out.println("Now you have " + list.size() + " tasks in the list.");
+                                System.out.println ("--------------------------------------------------------");
+                            } else {
+                                System.out.println("Invalid event format.");
+                            }
+                        } else {
+                            System.out.println("Invalid event format.");
+                        }
+
+                    // list all added items if user command is "list"
                 } else if(userCommand.equalsIgnoreCase("list")){
                     System.out.println ("--------------------------------------------------------");
                     System.out.println ("Here are the tasks in your list:");
                     for (int i = 0; i < list.size(); i++) {
                         Task t = list.get(i);
-                        System.out.println((i + 1) + " " + t.getStatusIcon() + " " + t.getDescription());
+                        System.out.println((i + 1) +". " + t.toString() );
                     }
                     System.out.println ("--------------------------------------------------------");
                     continue;
