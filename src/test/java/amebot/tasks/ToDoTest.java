2f859cead576b48755ab3b2fd1786e7bc2d78a1a
hweeteng <e0895876@u.nus.edu>
2023-11-11 20:02:28 +0800

    Update JUnit tests

--- src/test/java/amebot/tasks/ToDoTest.java
+++ src/test/java/amebot/tasks/ToDoTest.java
@@ -1,8 +1,28 @@
 package amebot.tasks;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ToDoTest {
+    Task task;
+
+    @BeforeEach
+    public void addTask_ToDo() {
+        this.task = new ToDo(false, "test todo task~");
+        assertEquals("[TODO] [ ] TEST TODO TASK~", this.task.getTask());
+    }
+
+    @Test
+    public void markTask_ToDo() {
+        this.task.setStatusAsMarked();
+        assertEquals("[TODO] [✓] TEST TODO TASK~", this.task.getTask());
+    }
+
+    @Test
+    public void unmarkTask_ToDo() {
+        this.task.setStatusAsUnmarked();
+        assertEquals("[TODO] [ ] TEST TODO TASK~", this.task.getTask());
+    }
 }
